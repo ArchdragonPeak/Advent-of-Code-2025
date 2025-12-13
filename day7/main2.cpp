5798b11d91dbe43e56c7b6e627c789f80c0fb3eb
ArchdragonPeak <erzdrachengipfel@gmail.com>
2025-12-13 17:34:42 +0100

    DAY 7 - PART 2

--- day7/main2.cpp
+++ day7/main2.cpp
@@ -2,69 +2,150 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <memory>
 
 using std::string;
 using std::vector;
 using std::cout;
+using std::shared_ptr;
+using std::make_shared;
 
-// we only get the part of the tree (wait is this a pun?)
-// that we want to traverse, so the current line and downwards
-// we don't care about the upper parts
-// ideally we get a copy so we dont destroy our precious tree
+struct Tuple
+{
+  size_t x;
+  size_t y;
+};
+
+struct Node
+{
+  Tuple pos;
+  shared_ptr<Node> left;
+  shared_ptr<Node> right;
+  size_t value = 0;
+};
 
 size_t counter = 0;
 
-vector<string> goDown(vector<string> &lines, size_t lineCursor, size_t vectorCursor)
+void traverse(shared_ptr<Node>& parent)
 {
-  //cout << "Cursor: " << lineCursor << "\n";
+  cout << "Traversing: " << parent->pos.x << ", " << parent->pos.y << "\n";
   
-  for(size_t i = vectorCursor; i < lines.size();i++)
+  //if(parent->value != 0) return;
+  counter++;
+  // go left
+  if(parent->left != nullptr)
   {
-    if(lines[i][lineCursor] == '^' || lines[i][lineCursor] == 'X')
+    if(parent->left->value == 0)
     {
-      //vector<string> cpy;
-      // go left
-      if(lineCursor > 0)
-      {
-        //cpy.assign(lines.begin(), lines.end());
-        //cpy[i][lineCursor-1] = '|';
-        goDown(lines, lineCursor-1, i);
-      }
-      // then go right
-      if(lineCursor < lines[i].size()-1)
-      {
-        //cpy.assign(lines.begin(), lines.end());
-        //cpy[i][lineCursor+1] = '|';
-        return goDown(lines, lineCursor+1, i);
-      }
-      
+      traverse(parent->left);
+      parent->value += parent->left->value;
     }
     else
     {
-      lines[i][lineCursor] = '|';
+      parent->value += parent->left->value;
     }
   }
-  
-  counter++;
-  if(counter % 1 == 0)
+  else
   {
-    for(string line : lines)
-    {
-      cout << line << "\n";
-    }
-    cout << "Counter: " << counter << "\n";
+    cout << "no left child" << "\n";
+    parent->value++;
   }
   
-  return lines;
+  // go right
+  if(parent->right != nullptr)
+  {
+    if(parent->right->value == 0)
+    {
+    traverse(parent->right);
+    parent->value += parent->right->value;
+    }
+    else
+    {
+      parent->value += parent->right->value;
+    }
+  }
+  else
+  {
+    cout << "no right child" << "\n";
+    parent->value++;
+  }
+  cout << "My value is: " << parent->value << "\n";
 }
+
+void getChildren(vector<string> lines, vector<shared_ptr<Node>>& treeList, shared_ptr<Node>& node)
+{
+  const size_t y = node->pos.y;
+  //cout << node-> pos.x <<", " << y << "\n";
+  
+  // alogrithm to get the two children of a node
+  for(size_t i = node->pos.x; i < lines.size();i++)
+  {
+    //cout << "i: " << i << " y: " << y << "\n";
+    if(y>0 && node->left == nullptr)
+    {
+      if(lines[i][y-1] == '^')
+      {
+        // left node
+        for(shared_ptr<Node>& child:treeList)
+        {
+          if(child->pos.x == i && child->pos.y == node->pos.y-1)
+          {
+            cout << "Child Node (" << child->pos.x << ", " << child->pos.y
+            << ") found for (" << node->pos.x << ", " << node->pos.y << ")" << "\n";
+            node->left = child;
+            break;
+          }
+        }
+      }
+    }
+    
+    if(y<lines[i].size()-1 && node->right == nullptr)
+    {
+      if(lines[i][y+1] == '^')
+      {
+        // right node
+        for(shared_ptr<Node>& child:treeList)
+        {
+          if(child->pos.x == i && child->pos.y == node->pos.y+1)
+          {
+            cout << "Child Node (" << child->pos.x << ", " << child->pos.y
+            << ") found for (" << node->pos.x << ", " << node->pos.y << ")" << "\n";
+            node->right = child;
+            break;
+          }
+        }
+      }
+    }
+  }
+}
+
+// x: height, y: width
+void construct(vector<string> lines, vector<shared_ptr<Node>>& treeList)
+{
+  for(size_t i = 0; i < lines.size(); i++)
+  {
+    cout << lines[i] << " line: " << i << "\n";
+    for(size_t j = 0; j < lines[i].size(); j++)
+    {
+      if(lines[i][j] == '^')
+      {
+        shared_ptr<Node> node = make_shared<Node>();
+        node->pos = {i ,j};
+        //if(i == lines.size()-2) node->value = 2;
+        cout << "Node: (" << node->pos.x << ", " << node->pos.y << ") value: " << node->value << "\n";
+        treeList.push_back(std::move(node));
+      }
+    }
+  }
+}
+
 
 int main()
 {
-  std::fstream file("test");
+  std::fstream file("input");
   string line;
   vector<string> lines;
-  vector<vector<size_t>> visited;
-  
+  vector<shared_ptr<Node>> treeList;
   while(getline(file, line))
   {
     lines.push_back(line);
@@ -74,9 +155,27 @@ int main()
   size_t first = lines[0].find('S');
   lines[1][first] = '|';
   
-  vector<string> cpy;
-  cpy.assign(lines.begin()+2, lines.end());
-  goDown(cpy, first, 0);
+  // erasing multiple exists, but still
+  lines.erase(lines.begin());
+  lines.erase(lines.begin());
   
+  shared_ptr<Node> root = make_shared<Node>();
+  root->pos = {0, lines[0].find('^')};
+  construct(lines, treeList);
+  
+  // construct tree
+  for(shared_ptr<Node>& node : treeList)
+  {
+    getChildren(lines, treeList, node);
+  }
+  cout << treeList.size() << "\n";
+  // traverse tree
+  traverse(treeList[0]);
+  
+  
+  cout << "Root Node Value: " << treeList[0]->value << "\n";
+  
+  
+  cout << "---" << "\n";
   cout << counter << "\n";
 }
